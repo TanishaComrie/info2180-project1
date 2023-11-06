277335bdc5a65720a04aa74bf06026d4169486e3
TanishaComrie <tgcmax@gmail.com>
2023-11-05 19:11:44 -0700

    styles.css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    message();   
+} 
+
+function message(){
+    let e_mail = document.forms["email"].value;
+    if (x == ""){
+        return "Please enter a valid email address.";
+    }
+    else{
+        return "Thank you! Your email address `<example@address.com>` has been added to our mailing list!";
+
+    }
+        
+}
